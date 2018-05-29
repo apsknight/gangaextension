67991110250486904da064281a2bf410b0c0b5bd
Aman Pratap Singh <amanprtpsingh@gmail.com>
2018-05-29 19:40:04 +0530

    Kernel Extension and Kernel Ganga Job Monitor
    - gangamagic is now registering ganga magic in kernel.
    - %%ganga is used for specifying Ganga Job
    - gangajob recieve Ganga Job code and send info and status to frontend by comms.

--- extension/gangajob.py
+++ extension/gangajob.py
@@ -1,38 +1,82 @@
 from __future__ import print_function
-import ganga.ganga
-from inspect import ismodule as inspect_ismodule
-from importlib import import_module
-from inspect import ismodule as inspect_ismodule
-from importlib import import_module
-
-# Messaging
-import json
-import pickle
-
-# IPython
+import re
 from IPython.utils.io import capture_output
-from IPython.core.interactiveshell import InteractiveShell
 import time
 
-def run(raw_cell):
-    # ganga_shell = InteractiveShell()
-    # ganga_shell.call_pdb = False
-    # ganga_shell.pdb = False
+ganga_imported = False
+with capture_output() as ganga_import_output:
+    try:
+        import ganga.ganga
+        ganga_imported = True
+    except ImportError as e:
+        print("GangaMonitor: Unable to import Ganga in Python \n %s \n" % str(e))
 
-    # shell_output = ""
+if ganga_imported:
+    print("GangaMonitor: Ganga Imported succesfully")
 
-    # with capture_output() as io:
-    #     _ = ganga_shell.run_cell("import ganga.ganga", silent=True, shell_futures=False)
-    #     __ = ganga_shell.run_cell(raw_cell, silent=True, shell_futures=False)
-    # ganga_ns = {}
-    # exec("import ganga.ganga") in ganga_ns
-    exec(raw_cell)
-    exec("job_obj = j")
-    print(job_obj.status)
+class GangaMonitor:
+    def __init__(self, ipython):
+        self.ipython = ipython
+    
+    def __handle_incoming_msg(self, msg):
+        print("Message recieved from frontend: \n %s \n" % str(msg))
 
-    while(job_obj.status == "submitted"):
-        print(job_obj.status)
-        time.sleep(2)
-    print(job_obj.status)
-    print(job_obj.id)
+    def register_comm(self):
+        self.ipython.kernel.comm_manager.register_target("GangaMonitor", self.comm_target)
+    
+    def comm_target(self, comm, msg):
+        print("Comm Opened: \n %s \n" % str(msg))
+        self.comm = comm
+
+        @self.comm.on_msg
+        def _recv(msg):
+            self.__handle_incoming_msg(msg)
+        self.comm.send({"msgtype": "commopen"})
+
+    def send(self, msg):
+        self.comm.send(msg)
+
+    def extract_job_obj(self, code): # Handle not found error
+        regex = r"(\w+)\s*=\s*ganga.Job\(\)"
+        matches = re.finditer(regex, code, re.MULTILINE)
+
+        for match in matches:
+            obj_name = match.group(1)
         
+        return str(obj_name)
+
+    def run(self, raw_cell):
+        job_obj_name = self.extract_job_obj(raw_cell)
+        mirror_code = "job_obj = %s" % job_obj_name
+
+        try:
+            with capture_output() as ganga_job_output:
+                exec(raw_cell)
+                ganga.runMonitoring()
+                print("Monitoring ON")
+        except Exception as e:
+            print("GangaMonitor: %s" % str(e))
+        else:
+            exec(mirror_code)
+            job_info = {
+                "msgtype": "jobinfo",
+                "id": job_obj.id,
+                "name": str(job_obj.name),
+                "backend": str(job_obj.backend.__class__.__name__),
+                "subjobs": len(job_obj.subjobs),
+                "status": "submitted",
+                "job_submission_time": str(job_obj.time.submitting())
+            }
+            self.send(job_info)
+            while True:
+                job_status = {
+                    "msgtype": "jobstatus",
+                    "id": job_obj.id,
+                    "status": str(job_obj.status),
+                    "duration": str(job_obj.time.runtime())
+                }
+                self.send(job_status)
+                if (job_status["status"] == "completed"):
+                    break
+                time.sleep(10)
+
--- extension/gangamagic.py
+++ extension/gangamagic.py
@@ -2,29 +2,26 @@ from __future__ import print_function
 from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic, line_cell_magic)
 from IPython.core.magic_arguments import (argument, magic_arguments,
     parse_argstring)
-from gangajob import run
+from gangajob import GangaMonitor
 
 @magics_class
 class Ganga(Magics):
     """"""
-    # @magic_arguments()
-    # @argument("-n", "--name", type=str, help="Name of the Job")
-    # @argument("-b", "--backend", type=str, help="Choose backend")
-    # @argument("-s", "--splitter", type=str, help="Choose backend")
-    # @argument("--application", type=str, help="Application to exectute")
 
     @line_cell_magic
     def ganga(self, line, cell):
-        # args = parse_argstring(self.ganga, line)
-        # print(args.name, args.backend)
+        monitor.send({"msgtype": "magic_execution_start"})
         code = ""
         if cell is None:
             code = line
         else:
             code = cell
-
-        run(code)
+            
+        monitor.run(code)
 
 
 def load_ipython_extension(ipython):
+    global monitor
+    monitor = GangaMonitor(ipython)
+    monitor.register_comm()
     ipython.register_magics(Ganga)
