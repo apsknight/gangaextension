cbdc73e563e235cc0b67592d0e8e8b5910aefe20
Aman Pratap Singh <amanprtpsingh@gmail.com>
2018-05-29 19:43:11 +0530

    Frontend Ganga Monior for starting comm and redirecting messages

---
+++ extension/frontend/gangamonitor.js
@@ -0,0 +1,108 @@
+/**
+ * Definiton of GangaMonitor singleton object for dealing communications with kernel.
+ * @module GangaMonitor
+ */
+
+// import Jupyter from 'base/js/namespace';  // Module for Jupyter frontend APIs
+// import events from 'base/js/events';	  // Module for Jupyter Notebook events
+define([
+    'base/js/namespace', 'require', 'base/js/events', './cellqueue', './displaymonitor'
+], function(
+    Jupyter, requirejs, events, currentcell, displaymonitor
+) {
+    /**
+     * @constructor - GangaMonitor object's constructor
+     */
+    function GangaMonitor() {
+
+        // Communication object with kernl.
+        this.comm = null;
+        this.cell = null;
+        this.startComm();
+        // Fix Kernel interruption/restarting
+        // events.on('kernel_connected.Kernel', $.proxy(this.startComm, this))
+    }
+
+    /**
+     * Start Communication with kerel using Comm API.
+     * Closes any existing communication.
+     */
+    GangaMonitor.prototype.startComm = function () {
+        if (this.comm) {
+            this.comm.close();
+        }
+        var that = this;
+        console.log("GangaMonitor: Starting a connection with Kernel.");
+
+        if (Jupyter.notebook.kernel) {
+            this.comm = Jupyter.notebook.kernel.comm_manager.new_comm('GangaMonitor',
+                        { 'msgtype': 'openfromfrontend' });
+            
+            // Comm Message handlers.
+            this.comm.on_msg($.proxy(that.comm_msg, that));
+            this.comm.on_close($.proxy(that.comm_close, that));
+        }
+        else {
+            console.error('GangaMonitor: Unable to start communication. Kenel not found!');
+        }
+    }
+
+    /**
+     * Called when a message is recieved from kernel.
+     * @param {object} msg - JSON recieve message object.
+     */
+    GangaMonitor.prototype.comm_msg = function (msg) {
+        console.log('GangaMonitor: Recieved message!', msg.content.data);
+        this.handle_message(msg);
+    }
+
+    /**
+     * Called when communication with kernel is closed.
+     * @param {object} msg - JSON close message object.
+     */
+    GangaMonitor.prototype.comm_close = function (msg) {
+        console.log('GangaMonitor: Comm Close Message:', msg);
+    }
+
+    GangaMonitor.prototype.handle_message = function (msg) {
+        var data = msg.content.data;
+        if (!data.msgtype) {
+            console.warn('GangaMonitor: Unknown Data Recieved');
+        }
+        switch(data.msgtype) {
+            case "magic_execution_start":
+                console.log("Magic Execution Start");
+                this.cell = currentcell.getRunningCell();
+                console.log('This cell', this.cell);
+                break;
+            case "jobinfo":
+                console.log('GangaMonitor: Job Info Recieved');
+                this.job_info_recieved(data);
+                break;
+            case "jobstatus":
+                console.log('GangaMonitor: Job Status Recieved');
+                this.job_status_recieved(data);
+                break;
+        }
+    }
+
+    GangaMonitor.prototype.job_info_recieved = function (data) {
+        var cell = this.cell;
+        if (cell == null) {
+            console.error('GangaMonitor: Job Started with no running cell');
+            return;
+        }
+        // console.log('Cell:', cell);
+        console.log('GangaMonitor: Job started at cell', cell.cell_id, data);
+        var dismonitor = new displaymonitor.DisplayMonitor(cell, data);
+        // displaymonitor.initializeDisplay();
+    }
+
+    GangaMonitor.prototype.job_status_recieved = function (data) {
+        console.log(data);
+    }
+
+    return {
+        'GangaMonitor': GangaMonitor
+    };
+});
