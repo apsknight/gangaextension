b8b72897e304ea1f73bc6009f3db2232bb6c8d8e
Aman Pratap Singh <amanprtpsingh@gmail.com>
2018-05-29 19:47:44 +0530

    Delete unnecessary module

--- extension/backend/__init__.py
+++
@@ -1,5 +0,0 @@
-def _jupyter_server_extension_paths():
-    """This function is used by 'jupyter serverextension' command to install Jupyter Server Extension"""
-    return [{
-        "module": "backend.serverextension"
-}]
