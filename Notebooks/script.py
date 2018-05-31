6d9568a05d36d54651f65bb652350eb33add5651
Aman Pratap Singh <amanprtpsingh@gmail.com>
2018-05-31 22:13:29 +0530

    Add Test Notebooks and scripts

---
+++ Notebooks/script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import requests
+import re, os, sys
+
+urls = sys.argv[1:]
+
+for url in urls:
+    req = requests.get(url)
+    text = req.text
+
+    word1 = 'island'
+    word2 = 'islands'
+
+    count1 = sum(1 for found in re.finditer(r'\b%s\b' % word1, text, re.I))
+    count2 = sum(1 for found in re.finditer(r'\b%s\b' % word2, text, re.I))
+
+    f = open('islands.txt', 'a')
+    f.write('{} {} {} {} {} \n'.format(url, word1, count1, word2, count2))
+    f.close()
