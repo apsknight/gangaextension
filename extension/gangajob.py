c9be487973915d4164341ffea439fbba43f212e2
Aman Pratap Singh <amanprtpsingh@gmail.com>
2018-06-03 00:36:46 +0530

    Correct blunder push

--- extension/gangajob.py
+++ extension/gangajob.py
@@ -3,6 +3,7 @@ import re
 from IPython.utils.io import capture_output
 import time
 
+# Import Ganga
 ganga_imported = False
 with capture_output() as ganga_import_output:
     try:
@@ -15,6 +16,9 @@ if ganga_imported:
     print("GangaMonitor: Ganga Imported succesfully")
 
 class GangaMonitor:
+    """
+    Main singleton object for running Ganga as Python API.
+    """
     def __init__(self, ipython):
         self.ipython = ipython
     
@@ -22,8 +26,19 @@ class GangaMonitor:
         print("Message recieved from frontend: \n %s \n" % str(msg))
         data = msg["content"]["data"]
         if data["msgtype"] == "cancel":
-            ganga.jobs[int(data.id)].kill()
+            ganga.jobs[int(data["id"])].kill()
+            job_obj = self.job_obj
+            job_status = {
+                "msgtype": "jobstatus",
+                "id": job_obj.id,
+                "status": str(job_obj.status),
+            }
+            if len(job_obj.subjobs) > 0:
+                job_status.update({"subjob_status": {}})
+                for sj in job_obj.subjobs:
+                    job_status["subjob_status"][str(sj.id)] = str(sj.status)
 
+            self.send(job_status)
 
     def register_comm(self):
         self.ipython.kernel.comm_manager.register_target("GangaMonitor", self.comm_target)
@@ -49,6 +64,50 @@ class GangaMonitor:
         
         return str(obj_name)
 
+    def send_job_info(self):
+        job_obj = self.job_obj
+        job_info = {
+                "msgtype": "jobinfo",
+                "id": job_obj.id,
+                "name": str(job_obj.name),
+                "backend": str(job_obj.backend.__class__.__name__),
+                "subjobs": len(job_obj.subjobs),
+                "status": "submitted",
+                "job_submission_time": str(job_obj.time.submitting())[:19],
+                "application": str(job_obj.application).split()[0],
+                "splitter": str(job_obj.splitter).split()[0],             
+            }
+        if job_info["subjobs"] > 0:
+            job_info.update({"subjob_submission_time": {}})
+            for sj in job_obj.subjobs:
+                job_info["subjob_submission_time"][str(sj.id)] = str(sj.time.submitting())[:19]
+            
+        self.send(job_info)
+
+    def send_job_status(self):
+        job_obj = self.job_obj
+        endpoints = ["completed", "killed", "failed"]
+        while True:
+            ganga.runMonitoring()
+            job_status = {
+                "msgtype": "jobstatus",
+                "id": job_obj.id,
+                "status": str(job_obj.status),
+            }
+            if (job_status["status"] in endpoints):
+                job_status.update({"runtime": str(job_obj.time.runtime())})
+            if len(job_obj.subjobs) > 0:
+                job_status.update({"subjob_status": {}})
+                job_status.update({"subjob_runtime": {}})
+                for sj in job_obj.subjobs:
+                    job_status["subjob_status"][str(sj.id)] = str(sj.status)
+                    if (str(sj.status) in endpoints):
+                        job_status["subjob_runtime"][str(sj.id)] = str(sj.time.runtime())
+            time.sleep(1)
+            self.send(job_status)
+            if (job_status["status"] in endpoints):
+                break
+
     def run(self, raw_cell):
         job_obj_name = self.extract_job_obj(raw_cell)
         mirror_code = "job_obj = %s" % job_obj_name
@@ -57,36 +116,12 @@ class GangaMonitor:
             with capture_output() as ganga_job_output:
                 exec(raw_cell)
                 ganga.runMonitoring()
-                print("Monitoring ON")
+                print("GangaMonitor: Monitoring ON")
         except Exception as e:
             print("GangaMonitor: %s" % str(e))
         else:
             exec(mirror_code)
-            job_info = {
-                "msgtype": "jobinfo",
-                "id": job_obj.id,
-                "name": str(job_obj.name),
-                "backend": str(job_obj.backend.__class__.__name__),
-                "subjobs": len(job_obj.subjobs),
-                "status": "submitted",
-                "job_submission_time": str(job_obj.time.submitting())[:19],
-                "application": str(job_obj.application).split()[0]
-            }
-            self.send(job_info)
-            endpoints = ["completed", "killed", "failed"]
-            while True:
-                ganga.runMonitoring()
-                job_status = {
-                    "msgtype": "jobstatus",
-                    "id": job_obj.id,
-                    "status": str(job_obj.status),
-                    "duration": str(job_obj.time.submitting())[:19]
-                }
-                if job_info["subjobs"] > 0:
-                    job_status.update({"subjob_status": {}})
-                    for sj in job_obj.subjobs:
-                        job_status["subjob_status"][str(sj.id)] = str(sj.status)
-                time.sleep(1)
-                self.send(job_status)
-                if (job_status["status"] in endpoints):
-                    break
+            self.job_obj = job_obj
+            self.send_job_info()
+            self.send_job_status()
+            
--- extension/gangamagic.py
+++ extension/gangamagic.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
-from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic, line_cell_magic)
-from IPython.core.magic_arguments import (argument, magic_arguments,
-    parse_argstring)
+from IPython.core.magic import (Magics, magics_class, line_cell_magic)
 from gangajob import GangaMonitor
+from threading import Thread
 
 @magics_class
 class Ganga(Magics):
-    """"""
-
+    """
+    Class for registering '%%ganga' magic cell in kernel.
+    """
     @line_cell_magic
     def ganga(self, line, cell):
         monitor.send({"msgtype": "magic_execution_start"})
@@ -16,10 +16,12 @@ class Ganga(Magics):
             code = line
         else:
             code = cell
-            
-        monitor.run(code)
+        
+        # Create new thread to avoid kernel interruption while Job is running.
+        ganga_thread = Thread(target=monitor.run, args=(code,))
+        ganga_thread.start()
 
-
+# Default ipython entrypoint for kernel extension.
 def load_ipython_extension(ipython):
     global monitor
     monitor = GangaMonitor(ipython)
