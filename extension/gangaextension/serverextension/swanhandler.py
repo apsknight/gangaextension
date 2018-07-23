0496890a4d1509866b33e4d20d6f578c7fc091a2
Aman Pratap Singh <amanprtpsingh@gmail.com>
2018-07-24 00:37:12 +0530

    Fix Indentation

--- extension/gangaextension/serverextension/swanhandler.py
+++ extension/gangaextension/serverextension/swanhandler.py
@@ -2,9 +2,8 @@ from notebook.base.handlers import IPythonHandler
 
 class SwanHandler(IPythonHandler):
     def get(self):
-        self.write(self.render_template(('gangapage.html',
-					                     page_title='Ganga Jobs',
-					                     ganga_page='True',
-					                     terminals_available=self.settings['terminals_available'],
+        self.write(self.render_template('gangapage.html',
+					page_title='Ganga Jobs',
+					ganga_page='True',
+					terminals_available=self.settings['terminals_available'],
                                         ))
-
