2332d2b579646f328580f6ff1850d9b7452b0a65
Aman Pratap Singh <amanprtpsingh@gmail.com>
2018-08-04 23:35:43 +0530

    Also install nbextension on pip install

--- extension/gangaextension/__init__.py
+++ extension/gangaextension/__init__.py
@@ -1,3 +1,23 @@
+def _jupyter_nbextension_paths():
+    """Used by 'jupyter nbextension' command to install frontend extension"""
+    return [dict(
+        section="notebook",
+        # the path is relative to the `extension` directory
+        src="nbextension",
+        # directory in the `nbextension/` namespace
+        dest="gangaextension",
+        # _also_ in the `nbextension/` namespace
+        require="gangaextension/extension"),
+
+        dict(
+        section="tree",
+        # the path is relative to the `extension` directory
+        src="nbextension",
+        # directory in the `nbextension/` namespace
+        dest="gangaextension",
+        # _also_ in the `nbextension/` namespace
+        require="gangaextension/extension")]
+
 def _jupyter_server_extension_paths():
     """Used by "jupyter serverextension" command to install web server extension'"""
     return [{
