3b558e9cd0ac2b737c50f218087adabad7c75354
Aman Pratap Singh <amanprtpsingh@gmail.com>
2018-08-09 22:38:44 +0530

    Fix #26: Add unified thread to track status of all jobs submitted in a kernel session

--- extension/gangaextension/kernelextension/gangajob.py
+++ extension/gangaextension/kernelextension/gangajob.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import re
 from IPython.utils.io import capture_output
 import time
@@ -7,39 +6,55 @@ from threading import Thread
 class GangaMonitor:
     """
     Main singleton object for running Ganga as Python API.
+    This is the mail class for kernel extension which handles actual submission of Jobs,
+    sends it info and status to frontend. Jobs are not monitored in kernel but monitored in 
+    server extension.
+
+    This class registers a Comm connection using Jupyter Comm API, which communicates with 
+    frontend nbextension (gangamonitor.js) and using this connection it sends all the Jobs info and status.
     """
     def __init__(self, ipython):
         self.ipython = ipython
         self.cell = None
+        # Import Ganga Module in Kernel namespace
         self.ipython.run_code("import ganga.ganga")
+        # Import default Ganga Namespace in Kernel Namespace
         self.ipython.run_code("from ganga import *")
+        # Disable Monitoring as Jobs won't be monitored here.
         self.ipython.run_code("disableMonitoring()")
+        # List of active Jobs which needs to be tracked and status to be sent to nbextension
+        self.active_jobs = list()
+        # Endpoints on which a Job finishes
+        self.endpoints = ["completed", "killed", "failed"]
+        # Create and start an unified thread to send status of all active Jobs to frontend.
+        status_thread = Thread(target=self.job_status_tracker, args=())
+        status_thread.start()
 
     def __handle_incoming_msg(self, msg):
-        print("Message recieved from frontend: \n %s \n" % str(msg))
-
+        '''
+        Handle incoming messages from nbextension (Gangamonitor.js)
+        '''
+        # print("Message recieved from frontend: \n %s \n" % str(msg))
         data = msg["content"]["data"]
-
-        # if data["msgtype"] == "askinfo":
-        #     self.send_job_info(int(data["id"]), data["cell"])
         
-        # If resubmission is requested, resubmit job and start monitoring thread.
+        # If resubmission is requested, resubmit job and append it to active jobs list.
         if data["msgtype"] == "resubmit":
             id = int(data["id"])
             self.ipython.run_code("jobs[%s].resubmit()" % id)
             self.send_job_info(int(data["id"]), data["cell"])
-            self.send_job_status(int(data["id"]), data["cell"])
+            self.active_jobs.append(tuple((int(data["id"]), data["cell"])))
 
-        # If cellinfo is recieved, store it.
+        # If cellinfo is recieved, store it. This is the cell id of the cell from which
+        # current process was started, self.cell get overwritten whenever a new job submits.
         if data["msgtype"] == "cellinfo":
             self.cell = data["cell_id"]
 
     def register_comm(self):
-        # This method is used for registering comm
+        # This method is used for registering comm in kernel.
         self.ipython.kernel.comm_manager.register_target("GangaMonitor", self.comm_target)
     
     def comm_target(self, comm, msg):
-        print("Comm Opened: \n %s \n" % str(msg))
+        # print("Comm Opened: \n %s \n" % str(msg))
         self.comm = comm
 
         @self.comm.on_msg
@@ -78,6 +93,7 @@ class GangaMonitor:
             matchCount = matchCount + 1
         
         if matchCount > 1:
+            # This is a design decision that only one Job can be defined per cell.
             self.ipython.run_code('raise Exception("Only one Ganga Job can be defined in a single cell magic.")')
             return False
 
@@ -112,54 +128,80 @@ class GangaMonitor:
             
         self.send(job_info)
 
-    def send_job_status(self, id, cell_id):
+    def fetch_job_status(self, id, cell_id):
         """
-        Send status of job to frontend
+        Fetch current status of job with id `id` from kernel.
+        Note: Only load status of Jobs which are in submitted or running stage from disk,
+              loading status of new jobs from disk will result in overwriting of Job object by
+              the default data present in disk. This is not the case in submitted state as the disk
+              is flushed with latest data.
         """
         self.ipython.run_code('job_obj = jobs[%s]' % id)
         job_obj = self.ipython.user_ns['job_obj']
 
-        endpoints = ["completed", "killed", "failed"]
-
-        while True:
-            # Run infinite loop until endpoint is reached
-            if str(job_obj) == "submitted" or str(job_obj) == "running":
-                self.ipython.run_code('reloadJob(%s)' % id)
-            self.ipython.run_code('job_obj = jobs[%s]' % id)
-            job_obj = self.ipython.user_ns['job_obj']
-            
+        if str(job_obj.status) == "new" or str(job_obj.status) == "submitting":
             job_status = {
                 "msgtype": "jobstatus",
                 "id": job_obj.id,
                 "cell_id": cell_id,
                 "status": str(job_obj.status),
-                "job_submission_time": str(job_obj.time.submitting())[:19],
             }
+            return job_status
 
-            if len(job_obj.subjobs) > 0:
-                job_status.update({"subjob_status": {}})
-                job_status.update({"subjob_runtime": {}})
-                for sj in job_obj.subjobs:
-                    job_status["subjob_status"][str(sj.id)] = str(sj.status)
-                #     if (str(sj.status) == "completed"):
-                #         job_status["subjob_runtime"][str(sj.id)] = str(sj.time.runtime())
+        if str(job_obj.status) == "submitted" or str(job_obj.status) == "running":
+            # This method `reloadJob` seems not to update the status of subjobs. This needs a Ganga Fix
+            # Until then use Job data as Subjob data.
+            self.ipython.run_code('reloadJob(%s)' % id)
+            self.ipython.run_code('job_obj = jobs[%s]' % id)
+            job_obj = self.ipython.user_ns['job_obj']
+                        
+        job_status = {
+            "msgtype": "jobstatus",
+            "id": job_obj.id,
+            "cell_id": cell_id,
+            "status": str(job_obj.status),
+            "job_submission_time": str(job_obj.time.submitting())[:19],
+        }
 
-            if (job_status["status"] == "completed"):
-                job_status.update({"runtime": str(job_obj.time.runtime())})
-                for sj in job_obj.subjobs:
-                    job_status["subjob_status"][str(sj.id)] = job_status["status"]
-                    # if (str(sj.status) == "completed"):
-                    job_status["subjob_runtime"][str(sj.id)] = job_status["runtime"]
+        if len(job_obj.subjobs) > 0:
+            job_status.update({"subjob_status": {}})
+            job_status.update({"subjob_runtime": {}})
+            for sj in job_obj.subjobs:
+                job_status["subjob_status"][str(sj.id)] = str(sj.status)
+            #     if (str(sj.status) == "completed"):
+            #         job_status["subjob_runtime"][str(sj.id)] = str(sj.time.runtime())
 
-            self.send(job_status)
+        if (job_status["status"] == "completed"):
+            job_status.update({"runtime": str(job_obj.time.runtime())})
+            for sj in job_obj.subjobs:
+                job_status["subjob_status"][str(sj.id)] = job_status["status"]
+                # if (str(sj.status) == "completed"):
+                job_status["subjob_runtime"][str(sj.id)] = job_status["runtime"]
 
+        return job_status
 
-            # If endpoint is reached then break the loop.
-            if (job_status["status"] in endpoints):
-                break
+    def send_status(self):
+        # Send status of all active Jobs to frontend
+        completed_jobs = list()
+        for jobid, cellid in self.active_jobs:
+            status = self.fetch_job_status(jobid, cellid)
+            # If Job is completed remove it from active jobs.
+            if status["status"] in self.endpoints:
+                completed_jobs.append(tuple((jobid, cellid)))
+            self.send(status)
 
-            # Send Job status to frontend every 10 seconds
-            time.sleep(10)
+        new_active_jobs = list()
+        for job_tuple in self.active_jobs:
+            if job_tuple not in completed_jobs:
+                new_active_jobs.append(job_tuple)
+
+        self.active_jobs = new_active_jobs
+                
+    def job_status_tracker(self):
+        # Periodically send status of active Jobs to frontend.
+        while True:
+            self.send_status()
+            time.sleep(15)
 
     def run(self, raw_cell):
         """
@@ -172,11 +214,11 @@ class GangaMonitor:
             with capture_output() as ganga_job_output:
                 self.ipython.run_code(raw_cell)
         except Exception as e:
-            print("GangaMonitor: %s" % str(e))
+            self.ipython.run_code("print('GangaMonitor: %s')"  % str(e))
+            # print("GangaMonitor: %s" % str(e))
         else:
             jobid = self.ipython.user_ns[job_obj_name].id
             self.send_job_info(jobid, self.cell)
-            # Start new thread for sending status
-            status_thread = Thread(target=self.send_job_status, args=(jobid, self.cell))
-            status_thread.start()
+            # Append this Job to active jobs.
+            self.active_jobs.append(tuple((jobid, self.cell)))
             return [ganga_job_output]
